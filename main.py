940bfc99781ae25aea6294d724d8f35a9eefafec
MohamedElshewy <abdalluhmohamed@gmail.com>
2025-11-18 18:35:12 +0200

    implement some basic functions

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+# 0-based index
+import numpy as np
+
+
 def partial_pivoting(A,b,k):
     n = A.shape[0]
     max_index = k
@@ -10,11 +14,41 @@ def partial_pivoting(A,b,k):
         b[[k, max_index]] = b[[max_index, k]]
 
 def forward_elimination(A,b):
-    pass
+    n = A.shape[0]
+    for k in range(0, n-1):   # row traverse(find the row to pivot)
+        partial_pivoting(A,b,k)
+        for i in range(k + 1, n):  #row traverse(apply the elimination for specific row)
+            factor = A[i][k]/A[k][k]
+            for j in range(k, n):  # column traverse(apply the elimination for each element in this row)
+                A[i][j] = A[i][j] - (factor * A[k][j])
+            b[i] = b[i] - (factor * b[k])
+
+
+
 
 def forward_substitution(A,b):
-    pass
+    n = A.shape[0]
+    x = np.zeros(n)
+    x[0] = b[0] / A[0][0]
+    for i in range (1, n): # row traverse forward
+        sum = 0
+        for j in range(i + 1, -1, -1): # column traverse backward
+            sum = sum + (x[j] * A[i][j])
+
+        x[i] = (b[i] - sum) / A[i][i]
+
+
 
 def backward_substitution(A,b):
-    pass
+    n = A.shape[0]
+    x = np.zeros(n)
+    x[n - 1] = b[n - 1] / A[n-1][n-1]
+    for i in range(n - 2, 0, -1):  # row traverse backward
+        sum = 0
+        for j in range(i + 1, n):  # column traverse forward
+            sum = sum + (x[j] * A[i][j])
+
+        x[i] = (b[i] - sum) / A[i][i]
+
+
 
