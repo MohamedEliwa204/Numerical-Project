86dc381efa5c019e96e2878737a9d60dbf499932
MohamedElshewy <abdalluhmohamed@gmail.com>
2025-11-18 20:32:55 +0200

    implement Gauss elimination and Gauss Jordan

--- main.py
+++ main.py
@@ -1,54 +1,110 @@
 # 0-based index
 import numpy as np
+from abc import ABC, abstractmethod
+
+from typing_extensions import override
 
 
-def partial_pivoting(A,b,k):
-    n = A.shape[0]
-    max_index = k
-    for i in range(k,n):
-        if abs(A[i][k]) > abs(A[max_index][k]):
-            max_index = i
+class LineraSolver(ABC):
+    def __init__(self, A, b, precision=4):
+        self.A = np.array(A, dtype=float)
+        self.b = np.array(b, dtype=float)
+        self.n = len(b)
+        self.precision = precision
 
-    if max_index != k:
-        A[[k, max_index]] = A[[max_index, k]]
-        b[[k, max_index]] = b[[max_index, k]]
-
-def forward_elimination(A,b):
-    n = A.shape[0]
-    for k in range(0, n-1):   # row traverse(find the row to pivot)
-        partial_pivoting(A,b,k)
-        for i in range(k + 1, n):  #row traverse(apply the elimination for specific row)
-            factor = A[i][k]/A[k][k]
-            for j in range(k, n):  # column traverse(apply the elimination for each element in this row)
-                A[i][j] = A[i][j] - (factor * A[k][j])
-            b[i] = b[i] - (factor * b[k])
+    @abstractmethod
+    def solve(self):
+        pass
 
 
+class DirectSolver(LineraSolver):
+    def partial_pivoting(self, A, b, k):
+        n = A.shape[0]
+        max_index = k
+        for i in range(k, n):
+            if abs(A[i][k]) > abs(A[max_index][k]):
+                max_index = i
+
+        if max_index != k:
+            A[[k, max_index]] = A[[max_index, k]]
+            b[[k, max_index]] = b[[max_index, k]]
+
+    def forward_elimination(self, A, b):
+        n = A.shape[0]
+        for k in range(0, n - 1):  # row traverse(find the row to pivot)
+            self.partial_pivoting(A, b, k)
+
+            if np.isclose(A[k][k], 0):  # Check for singularity
+                raise ValueError("Matrix is singular or near-singular.")
+            for i in range(k + 1, n):  # row traverse(apply the elimination for specific row)
+                factor = A[i][k] / A[k][k]
+                A[i, k:] = A[i, k:] - factor * A[k, k:]
+                b[i] = b[i] - (factor * b[k])
+
+    def backward_elimination(self, A, b):
+        n = A.shape[0]
+
+        for k in range(n - 1, -1, -1):
+            pivot = A[k][k]
+            A[k] = A[k] / pivot
+            b[k] = b[k] / pivot
+            for i in range(0, k):
+                factor = A[i][k]
+                A[i] = A[i] - factor * A[k]
+                b[i] = b[i] - factor * b[k]
+
+    def forward_substitution(self, L, b):
+        n = L.shape[0]
+        x = np.zeros(n)
+        x[0] = b[0] / L[0][0]
+        for i in range(1, n):  # row traverse forward
+            sum = 0
+            for j in range(0, i):  # column traverse backward
+                sum = sum + (x[j] * L[i][j])
+
+            x[i] = (b[i] - sum) / L[i][i]
+
+        return x
+
+    def backward_substitution(self, A, b):
+        n = A.shape[0]
+        x = np.zeros(n)
+        x[n - 1] = b[n - 1] / A[n - 1][n - 1]
+        for i in range(n - 2, -1, -1):  # row traverse backward
+            sum = 0
+            for j in range(i + 1, n):  # column traverse forward
+                sum = sum + (x[j] * A[i][j])
+
+            x[i] = (b[i] - sum) / A[i][i]
+
+        return x
 
 
-def forward_substitution(A,b):
-    n = A.shape[0]
-    x = np.zeros(n)
-    x[0] = b[0] / A[0][0]
-    for i in range (1, n): # row traverse forward
-        sum = 0
-        for j in range(0, i): # column traverse backward
-            sum = sum + (x[j] * A[i][j])
+class GaussElimination(DirectSolver):
 
-        x[i] = (b[i] - sum) / A[i][i]
+    @override
+    def solve(self):
+        A_bar = self.A.copy()
+        b_bar = self.b.copy()
 
-    return x
+        self.forward_elimination(A_bar, b_bar)
+        solution = self.backward_substitution(A_bar, b_bar)
+        return np.round(solution, decimals=self.precision)
 
 
-def backward_substitution(A,b):
-    n = A.shape[0]
-    x = np.zeros(n)
-    x[n - 1] = b[n - 1] / A[n-1][n-1]
-    for i in range(n - 2, -1, -1):  # row traverse backward
-        sum = 0
-        for j in range(i + 1, n):  # column traverse forward
-            sum = sum + (x[j] * A[i][j])
+class GaussJordan(DirectSolver):
+    @override
+    def solve(self):
+        A_bar = self.A.copy()
+        b_bar = self.b.copy()
 
-        x[i] = (b[i] - sum) / A[i][i]
+        self.forward_elimination(A_bar, b_bar)
+        self.backward_elimination(A_bar, b_bar)
+        solution = b_bar
 
-    return x
+        return np.round(solution, decimals=self.precision)
+
+
+class LUDecomposition(DirectSolver):
+    pass
+
