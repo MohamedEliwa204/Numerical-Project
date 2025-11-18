a33d4752b60c4ba528352a093b4f74258abbce2f
MohamedElshewy <abdalluhmohamed@gmail.com>
2025-11-18 18:38:31 +0200

    implement some basic functions

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def partial_pivoting(A,b,k):
     n = A.shape[0]
     max_index = k
     for i in range(k,n):
-        if abs(A[i][k] > abs(A[max_index][k])):
+        if abs(A[i][k]) > abs(A[max_index][k]):
             max_index = i
 
     if max_index != k:
@@ -32,23 +32,23 @@ def forward_substitution(A,b):
     x[0] = b[0] / A[0][0]
     for i in range (1, n): # row traverse forward
         sum = 0
-        for j in range(i + 1, -1, -1): # column traverse backward
+        for j in range(0, i): # column traverse backward
             sum = sum + (x[j] * A[i][j])
 
         x[i] = (b[i] - sum) / A[i][i]
 
+    return x
 
 
 def backward_substitution(A,b):
     n = A.shape[0]
     x = np.zeros(n)
     x[n - 1] = b[n - 1] / A[n-1][n-1]
-    for i in range(n - 2, 0, -1):  # row traverse backward
+    for i in range(n - 2, -1, -1):  # row traverse backward
         sum = 0
         for j in range(i + 1, n):  # column traverse forward
             sum = sum + (x[j] * A[i][j])
 
         x[i] = (b[i] - sum) / A[i][i]
 
-
-
+    return x
