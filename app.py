ab6a5603147830feb90410736ec5c4fd7ff56edd
MohamedElshewy <abdalluhmohamed@gmail.com>
2025-11-27 21:57:05 +0200

    manage the factory in server!

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-from sympy import factor
-# Import the factories
+
 from SolverFactory import NumericalSolverFactory, SymbolicSolverFactory, AbstractSolverFactory
 from mainSymbols import *
 from BackEnd.methods import *
