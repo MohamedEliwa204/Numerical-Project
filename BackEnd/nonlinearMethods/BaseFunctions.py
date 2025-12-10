c3b27b9319fec71daa3f7f701d3c58600c11cc56
Abdelrahman911 <abdelrahmanvalo1773@gmail.com>
2025-12-10 18:55:56 +0200

    Added endpoints for plotting and nonlinear solving (/plot & /solve_nonlinear)

---
+++ BackEnd/nonlinearMethods/BaseFunctions.py
@@ -0,0 +1,78 @@
+from dataclasses import asdict
+
+import numpy as np
+import math
+from abc import ABC, abstractmethod
+
+from sympy import parse_expr
+from sympy.parsing.sympy_parser import standard_transformations, implicit_multiplication_application, convert_xor
+from typing_extensions import override
+import sympy as sp
+from IterationStep import IterationStep
+
+
+class RootSolver(ABC):
+    def __init__(self, func_str, precision=10, max_iter=50, tolerance=1e-5):
+        self.func_str = func_str
+        self.precision = precision
+        self.max_iter = max_iter
+        self.tolerance = tolerance
+        self.steps = []
+
+        transformations = (standard_transformations + (implicit_multiplication_application, convert_xor))
+
+        try:
+            self.expr = parse_expr(self.func_str, transformations=transformations)
+        except:
+            raise ValueError("couldn't parse the expression")
+
+        symbols_found = list(self.expr.free_symbols)
+
+        if len(symbols_found) == 0:
+            self.variable = sp.Symbol('x')
+
+        elif len(symbols_found) == 1:
+            self.variable = symbols_found[0]
+        else:
+            raise ValueError(f"Too many variables found: {symbols_found}. Please use only one.")
+
+        self.f_calc = sp.lambdify(self.variable, self.expr, "numpy")
+
+    def f(self, value):
+        try:
+            return self.f_calc(value)
+        except Exception as e:
+            raise ValueError(f"Math Error at value {value}: {e}")
+
+    def calculate_error(self, x_new, x_old):
+        if x_new == 0:
+            return 0
+        else:
+            return abs(((x_new - x_old) / x_new) * 100)
+
+    def round_significant(self, value):
+        x = np.array(value, dtype=float)
+
+        if np.isscalar(x):
+            if not np.isfinite(x):
+                return x
+            if x == 0:
+                return 0
+            else:
+                digits = self.precision - int(np.floor(np.log10(abs(x)))) - 1
+                return round(x, digits)
+        else:
+            rounded_array = np.zeros_like(x)
+            for index, val in np.ndenumerate(x):
+                if not np.isfinite(val):
+                    rounded_array[index] = val
+                elif val == 0:
+                    rounded_array[index] = 0
+                else:
+                    digits = self.precision - int(np.floor(np.log10(abs(val)))) - 1
+                    rounded_array[index] = round(val, digits)
+            return rounded_array
+
+    @abstractmethod
+    def solve(self):
+        pass
